05459a98e74934d1ab48951061572e6d52928cac
shterneregen <shterneregen@gmail.com>
2020-07-02 01:36:55 +0300

    adding allocation service

--- src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
+++ src/main/java/guru/sfg/beer/inventory/service/repositories/BeerInventoryRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-
 public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
 
     List<BeerInventory> findAllByBeerId(UUID beerId);
+
+    List<BeerInventory> findAllByUpc(String upc);
 }
