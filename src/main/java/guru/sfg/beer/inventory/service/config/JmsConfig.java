07726b0ac56c5cd084602cef29b6cc3665006c89
shterneregen <shterneregen@gmail.com>
2020-07-05 21:41:28 +0300

    handle allocation request

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -10,14 +10,16 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
-    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+	public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+	public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+	public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
 
-    @Bean
-    public MessageConverter messageConverter(ObjectMapper objectMapper) {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        converter.setObjectMapper(objectMapper);
-        return converter;
-    }
+	@Bean
+	public MessageConverter messageConverter(ObjectMapper objectMapper) {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		converter.setObjectMapper(objectMapper);
+		return converter;
+	}
 }
